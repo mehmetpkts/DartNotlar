a0cc8aa67a19640e832147e9e77ecb55b63e9b88
Mehmet Pektas <mehmetpektasss18@gmail.com>
2024-07-06 19:39:35 +0300

    while_loop.dart dosyası tamamlandı.

---
+++ bin/while_loop.dart
@@ -0,0 +1,13 @@
+main(){
+
+  int s = 0;
+
+  while( s < 10 ){    // while loop'da koşul verilmek zorundadır.
+    print("s değerimiz :$s");
+
+    s++;                  // bu kısım unutulmamalıdır.
+
+  }
+
+
+}
